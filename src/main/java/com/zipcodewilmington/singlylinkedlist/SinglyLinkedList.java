7e18f8e5ae9e0375df44aaf02c1c35d3649c2c7a
Zachary Kitto <zmkitto11@gmail.com>
2021-07-31 08:16:51 -0400

    added basic features

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,181 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList <SomeType extends Comparable<SomeType>> {
+
+    private Node head;
+
+    class Node {
+
+        private SomeType element;
+        private Node next;
+
+        private Node() {}
+
+        public Node(SomeType element) {
+            this.element = element;
+        }
+
+        @Override
+        public String toString() {
+            return new StringBuilder()
+                    .append(element)
+                    .append(",")
+                    .append(" ")
+                    .append(next)
+                    .toString();
+        }
+    }
+
+    public void addElement(SomeType elementToAdd) {
+        Node tail = head;
+        if (head == null) {
+            head = new Node(elementToAdd);
+        }
+        else {
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = new Node(elementToAdd);
+        }
+    }
+
+    public SomeType getElementAtNodeIndex(Integer nodeIndex) {
+        Node tail = head;
+        if (nodeIndex == 0) {
+            return head.element;
+        }
+        else {
+            while (nodeIndex > 0) {
+                tail = tail.next;
+                nodeIndex--;
+            }
+        }
+        return tail.element;
+    }
+
+    public void removeElementAtIndex(Integer indexToRemove) {
+        int count = 1;
+        Node tail = head;
+        SinglyLinkedList<SomeType> listWithElementRemoved = new SinglyLinkedList<SomeType>();
+        if (head == null) {
+
+        }
+        else if (indexToRemove == 0) {
+            head.element = null;
+        }
+        else {
+            listWithElementRemoved.addElement(head.element);
+            while (tail.next != null) {
+                if (count != indexToRemove) {
+                    listWithElementRemoved.addElement(tail.next.element);
+                }
+                tail = tail.next;
+                count++;
+            }
+            head = listWithElementRemoved.head;
+        }
+    }
+
+    public boolean containsElement(SomeType elementToFind) {
+        Node tail = head;
+        if (head.element.equals(elementToFind)) {
+            return true;
+        }
+        else {
+            while (!tail.element.equals(elementToFind)) {
+                tail = tail.next;
+                if (tail.next == null) {
+                    break;
+                }
+            }
+            if (tail.element.equals(elementToFind)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer findElementIndex(SomeType elementToFind) {
+        int count = 0;
+        Node tail = head;
+        if (containsElement(elementToFind)) {
+            while (!tail.element.equals(elementToFind)) {
+                tail = tail.next;
+                count++;
+                if (tail.next == null) {
+                    break;
+                }
+            }
+            if (tail.element.equals(elementToFind)) {
+                return count;
+            }
+        }
+        return -1;
+    }
+
+    public Integer sizeOfList() {
+        int count = 1;
+        Node tail = head;
+        if (head == null) {
+            return 0;
+        }
+        else if (head.next == null) {
+            return 1;
+        }
+        else {
+            while (tail.next != null) {
+                tail = tail.next;
+                count++;
+            }
+            return count;
+        }
+    }
+
+    public SinglyLinkedList<SomeType> copyList() {
+        Node tail = head;
+        SinglyLinkedList<SomeType> newList = new SinglyLinkedList<SomeType>();
+        if (head == null) {
+            return newList;
+        }
+        newList.addElement(head.element);
+        while (tail.next != null) {
+            newList.addElement(tail.next.element);
+            tail = tail.next;
+        }
+        return newList;
+    }
+
+    public void sortList() {
+        SinglyLinkedList<SomeType> newList = new SinglyLinkedList<SomeType>();
+        for (int i = 0; i < this.sizeOfList(); i++) {
+            Node tail = head;
+            if (tail != null) {
+                while (tail.next != null) {
+                    if (tail.element.compareTo(tail.next.element) == -1) {
+                        newList.addElement(tail.element);
+                    } else {
+                        SomeType temp = tail.next.element;
+                        tail.next.element = tail.element;
+                        tail.element = temp;
+                        newList.addElement((tail.element));
+                    }
+                    tail = tail.next;
+                }
+            }
+        }
+    }
+
+    public int compareTo(SomeType object1, SomeType object2) {
+        return object1.compareTo(object2);
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    @Override
+    public String toString() {
+        return "SinglyLinkedList[ " + head + " " + ']';
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,229 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList();
+
+    @Test
+    public void nullaryConstructorTest() {
+        // Given
+        SinglyLinkedList.Node expectedHead = null;
+
+        // When
+        SinglyLinkedList.Node actualHead = singlyLinkedList.getHead();
+
+        // Then
+        Assert.assertEquals(expectedHead, actualHead);
+    }
+
+    @Test
+    public void addNodeTest_FirstElementInList() {
+        // Given
+        Integer expectedElementAdded = 6;
+
+        // When
+        singlyLinkedList.addElement(expectedElementAdded);
+        Integer actualElementAdded = singlyLinkedList.getElementAtNodeIndex(0);
+
+        // Then
+        Assert.assertEquals(expectedElementAdded, actualElementAdded);
+    }
+
+    @Test
+    public void addNodeTest() {
+        // Given
+        Integer expectedFirstElementAdded = 4;
+        Integer expectedSecondElementAdded = 6;
+        Integer expectedThirdElementAdded = 10;
+
+        // When
+        singlyLinkedList.addElement(expectedFirstElementAdded);
+        singlyLinkedList.addElement(expectedSecondElementAdded);
+        singlyLinkedList.addElement(expectedThirdElementAdded);
+        Integer actualFirstElementAdded = singlyLinkedList.getElementAtNodeIndex(0);
+        Integer actualSecondElementAdded = singlyLinkedList.getElementAtNodeIndex(1);
+        Integer actualThirdElementAdded = singlyLinkedList.getElementAtNodeIndex(2);
+
+        // Then
+        Assert.assertEquals(expectedFirstElementAdded, actualFirstElementAdded);
+        Assert.assertEquals(expectedSecondElementAdded, actualSecondElementAdded);
+        Assert.assertEquals(expectedThirdElementAdded, actualThirdElementAdded);
+    }
+
+    @Test
+    public void getElementAtNodeIndexTest() {
+        // Given
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(10);
+        Integer thirdNodeIndex = 2;
+        Integer expectedElement = 10;
+
+        // When
+        Integer actualElement = singlyLinkedList.getElementAtNodeIndex(thirdNodeIndex);
+
+
+        // Then
+        Assert.assertEquals(expectedElement, actualElement);
+    }
+
+    @Test
+    public void removeElementAtIndexTest() {
+        // Given
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(10);
+        singlyLinkedList.addElement(55);
+        singlyLinkedList.addElement(34);
+        singlyLinkedList.addElement(87);
+        singlyLinkedList.addElement(1);
+
+        // When
+        System.out.println(singlyLinkedList);
+        singlyLinkedList.removeElementAtIndex(4);
+        System.out.println(singlyLinkedList);
+        boolean doesContain10 = singlyLinkedList.containsElement(10);
+        boolean doesContain5 = singlyLinkedList.containsElement(5);
+        boolean doesContain34 = singlyLinkedList.containsElement(34);
+
+
+        // Then
+        Assert.assertTrue(doesContain10);
+        Assert.assertTrue(doesContain5);
+        Assert.assertFalse(doesContain34);
+    }
+
+    @Test
+    public void containsElementTest() {
+        // Given
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(10);
+        singlyLinkedList.addElement(55);
+        singlyLinkedList.addElement(34);
+        singlyLinkedList.addElement(87);
+        singlyLinkedList.addElement(1);
+
+        // When
+        boolean doesContain87 = singlyLinkedList.containsElement(87);
+        boolean doesContain10 = singlyLinkedList.containsElement(10);
+        boolean doesContain5 = singlyLinkedList.containsElement(5);
+
+        // Then
+        Assert.assertTrue(doesContain87);
+        Assert.assertTrue(doesContain10);
+        Assert.assertTrue(doesContain5);
+    }
+
+    @Test
+    public void findElementIndexTest() {
+        // Given
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(10);
+        singlyLinkedList.addElement(55);
+        singlyLinkedList.addElement(34);
+        singlyLinkedList.addElement(87);
+        singlyLinkedList.addElement(1);
+        Integer expectedIndexOfElement87 = 5;
+        Integer expectedIndexOfElement10 = 2;
+        Integer expectedIndexOfElement5 = 1;
+        Integer expectedIndexOfElement99 = -1;
+
+        // When
+        Integer actualIndexOfElement87 = singlyLinkedList.findElementIndex(87);
+        Integer actualIndexOfElement10 = singlyLinkedList.findElementIndex(10);
+        Integer actualIndexOfElement5 = singlyLinkedList.findElementIndex(5);
+        Integer actualIndexOfElement99 = singlyLinkedList.findElementIndex(99);
+
+        // Then
+        Assert.assertEquals(expectedIndexOfElement87, actualIndexOfElement87);
+        Assert.assertEquals(expectedIndexOfElement10, actualIndexOfElement10);
+        Assert.assertEquals(expectedIndexOfElement5, actualIndexOfElement5);
+        Assert.assertEquals(expectedIndexOfElement99, actualIndexOfElement99);
+    }
+
+    @Test
+    public void sizeOfListTest() {
+        // Given
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(10);
+        singlyLinkedList.addElement(55);
+        singlyLinkedList.addElement(34);
+        singlyLinkedList.addElement(87);
+        Integer expectedSizeOfList1 = 6;
+        Integer expectedSizeOfList2 = 7;
+
+        // When
+        Integer actualSizeOfList1 = singlyLinkedList.sizeOfList();
+        singlyLinkedList.addElement(1);
+        Integer actualSizeOfList2 = singlyLinkedList.sizeOfList();
+
+        // Then
+        Assert.assertEquals(expectedSizeOfList1, actualSizeOfList1);
+        Assert.assertEquals(expectedSizeOfList2, actualSizeOfList2);
+    }
+
+    @Test
+    public void copyListTest() {
+        // Given
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(5);
+        singlyLinkedList.addElement(10);
+        singlyLinkedList.addElement(55);
+        singlyLinkedList.addElement(34);
+        singlyLinkedList.addElement(87);
+        SinglyLinkedList<Integer> expectedList = new SinglyLinkedList<Integer>();
+        expectedList.addElement(4); expectedList.addElement(5); expectedList.addElement(10);
+        expectedList.addElement(55); expectedList.addElement(34); expectedList.addElement(87);
+
+        // When
+        SinglyLinkedList<Integer> actualList = singlyLinkedList.copyList();
+
+        // Then
+        for (int i = 0; i < singlyLinkedList.sizeOfList(); i++) {
+            Assert.assertEquals(expectedList.findElementIndex(i), actualList.findElementIndex(i));
+        }
+    }
+
+    @Test
+    public void sortListTest() {
+        // Given
+        singlyLinkedList.addElement(55);
+        singlyLinkedList.addElement(87);
+        singlyLinkedList.addElement(10);
+        singlyLinkedList.addElement(4);
+        singlyLinkedList.addElement(34);
+        singlyLinkedList.addElement(5);
+        Integer expectedFirstElement = 4;
+        Integer expectedSecondElement = 5;
+
+        // When
+        singlyLinkedList.sortList();
+        Integer actualFirstElement = singlyLinkedList.getElementAtNodeIndex(0);
+        Integer actualSecondElement = singlyLinkedList.getElementAtNodeIndex(1);
+
+        // Then
+        Assert.assertEquals(expectedFirstElement, actualFirstElement);
+        Assert.assertEquals(expectedSecondElement, actualSecondElement);
+    }
 }
